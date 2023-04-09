537817195375a6f5c83732e6fb845cb7b6f7d1a3
weeee9 <teletubby332@gmail.com>
2023-04-09 19:40:16 +0800

    feat: finished authentication page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './components/navigation/navigation.component';
 import Home from './routes/home/home.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import SignIn from './routes/authentication/authentication.component';
 
 const Shop = () => {
   return <h2>I am the shop page</h2>;
--- src/routes/authentication/authentication.component.tsx
+++ src/routes/authentication/authentication.component.tsx
@@ -1,10 +1,11 @@
 import SignInForm from '../../components/sign-in-form/sign-in-form.components';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
+import './authentication.styles.scss';
+
 const SignIn = () => {
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
     </div>
