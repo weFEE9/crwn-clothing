c843b918a45924f6238a2a2e06515b094a259b1f
weeee9 <teletubby332@gmail.com>
2023-04-16 11:24:47 +0800

    feat: add item to cart dropdown

---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,25 @@
+import { CartItem as Item } from '../../contexts/cart.context';
+
+import './cart-item.styles.scss';
+
+type MyProps = {
+  cartItem: Item;
+};
+
+const CartItem = ({ cartItem: cartItem }: MyProps) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+
+  return (
+    <div className='cart-item-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span>
+          {price} x ${quantity}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -1,13 +1,31 @@
-import { createContext, useState, Dispatch, SetStateAction } from 'react';
+import {
+  createContext,
+  useState,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from 'react';
+import { Product } from './products.context';
+import { toASCII } from 'punycode';
 
 type context = {
   isCartOpen: boolean;
   setIsCartOpen: Dispatch<SetStateAction<boolean>>;
+  cartItems: CartItem[];
+  addItemToCart: (productToAdd: Product) => void;
+  totalCount: number;
+};
+
+export type CartItem = Product & {
+  quantity: number;
 };
 
 export const CartContext = createContext<context>({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: (productToAdd: Product) => {},
+  totalCount: 0,
 });
 
 type MyProps = {
@@ -16,8 +34,65 @@ type MyProps = {
 
 export const CartProvider = ({ children }: MyProps) => {
   const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [totalCount, setTotalCount] = useState<number>(0);
 
-  const value: context = { isCartOpen, setIsCartOpen };
+  const addItemToCart = (productToAdd: Product) => {
+    const newItemList = addCartItem(cartItems, productToAdd);
+    setCartItems(newItemList);
+  };
+
+  useEffect(() => {
+    const totalCount = cartItems.reduce((total, item) => {
+      return total + item.quantity;
+    }, 0);
+
+    setTotalCount(totalCount);
+  }, [cartItems]);
+
+  const updateTotalCount = () => {
+    const totalCount = countCartItmes(cartItems);
+    setTotalCount(totalCount);
+  };
+
+  const value: context = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems: cartItems,
+    addItemToCart: addItemToCart,
+    totalCount: totalCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+const addCartItem = (
+  cartItems: CartItem[],
+  productToAdd: Product
+): CartItem[] => {
+  const cartItemExist = cartItems.find((item) => {
+    return item.id === productToAdd.id;
+  });
+
+  // find if cartItems contains productToAdd
+  if (cartItemExist) {
+    return cartItems.map((item) => {
+      return item.id === productToAdd.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item;
+    });
+  }
+
+  // return new array with modified cartItems w/ new cart item
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+const countCartItmes = (cartItems: CartItem[]): number => {
+  let count = 0;
+  cartItems.forEach((item) => {
+    count += item.quantity;
+  });
+
+  return count;
 };
