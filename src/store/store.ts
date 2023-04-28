447962b90d0b37c5d778e4269f84dd6611d15e66
weeee9 <teletubby332@gmail.com>
2023-04-28 15:27:53 +0800

    Redux persist (#13)
    * chore: add Redux Persist package to dependencies
    
    - Add `redux-persist` package to dependencies in `package.json`
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>
    
    * refactor: remove console logs from CategoriesPreview component
    
    - Remove console logs from CategoriesPreview component
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>
    
    * feat: refactor state persistence implementation and imports in `src` folder
    
    - Move `index.scss` import before `App` import in `src/index.tsx`
    - Add `PersistGate` component to `src/index.tsx`
    - Use `redux-persist` library for state persistence in `src/store/store.ts`
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>
    
    * refactor: improve logging middleware in store for non-prod environments
    
    - Remove logger middleware from store
    - Add conditional logger middleware to store for non-production environments
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>
    
    ---------
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,11 +4,32 @@ import {
   applyMiddleware,
 } from 'redux';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
-const middlewares = [logger];
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [];
+
+const isNotProductionEnvironment = process.env.NODE_ENV !== 'production';
+if (isNotProductionEnvironment) {
+  middlewares.push(logger);
+}
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
