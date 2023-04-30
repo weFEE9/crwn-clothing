7df12c61dc931f776312d99ed2ca54d34a241acc
weeee9 <teletubby332@gmail.com>
2023-04-30 11:19:28 +0800

    feat: add loading spinner to category components
    - Add a new file `spinner.component.tsx` and `spinner.styles.tsx`
    - Import `Spinner` component in `CategoriesPreview` and `Category` components
    - Add a loading spinner to `CategoriesPreview` and `Category` components
    - Add a new selector `selectCategoriesIsLoading` in `category.selector.ts`
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/routes/categories-preview/categories-preview.component.tsx
+++ src/routes/categories-preview/categories-preview.component.tsx
@@ -1,24 +1,33 @@
 import { useSelector } from 'react-redux';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from '../../store/categories/category.selector';
 import { Product } from '../../store/categories/category.types';
+import Spinner from '../../components/spinner/spinner.component';
 
 const CategoriesPreview = () => {
   const collectionsMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   return (
     <div className='category-preview-container'>
-      {Object.keys(collectionsMap).map((title) => {
-        const items = collectionsMap[title];
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(collectionsMap).map((title) => {
+          const items = collectionsMap[title];
 
-        return (
-          <CategoryPreview
-            key={title}
-            title={title}
-            products={items as Product[]}
-          ></CategoryPreview>
-        );
-      })}
+          return (
+            <CategoryPreview
+              key={title}
+              title={title}
+              products={items as Product[]}
+            ></CategoryPreview>
+          );
+        })
+      )}
     </div>
   );
 };
